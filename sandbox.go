954dc4303d5180ae7d894d76dbbda7733adb4847
Max Romanov <eds2013@bk.ru>
2021-08-22 11:54:38 +0300

    commit

--- sandbox.go
+++ sandbox.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	"log"
+	"net/http"
 )
 
 func main() {
-	//var out string = fmt.Sprintf("Number: \t %07d", 45)
-	//fmt.Println(out)
-
-	fmt.Println("Type the year you were born: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
-	fmt.Printf("You will be %d years old at he end of 2021", 2021-input)
+	http.HandleFunc("/", sayhello)           // Устанавливаем роутер
+	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
+
+func sayhello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Привет!")
+}
+
+//var out string = fmt.Sprintf("Number: \t %07d", 45)
+//fmt.Println(out)
+
+/*fmt.Println("Type the year you were born: ")
+scanner := bufio.NewScanner(os.Stdin)
+scanner.Scan()
+input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+fmt.Printf("You will be %d years old at he end of 2021", 2021-input)*/
